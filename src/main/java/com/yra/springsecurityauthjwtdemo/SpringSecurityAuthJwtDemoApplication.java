2567677b253f88485aa072b523d8986b1549551e
Roman Yarosh <yracompany@gmail.com>
2018-03-08 21:38:35 +0200

    RY

---
+++ src/main/java/com/yra/springsecurityauthjwtdemo/SpringSecurityAuthJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yra.springsecurityauthjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthJwtDemoApplication {
+
+	//https://o7planning.org/en/11677/secure-spring-boot-restful-service-using-auth0-jwt
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthJwtDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yra/springsecurityauthjwtdemo/SpringSecurityAuthJwtDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yra.springsecurityauthjwtdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityAuthJwtDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
